37d3e0f233d4b3440bade43c0d6ca4f54ec3e797
katuzny <katuznychristophe@gmail.fr>
2019-04-09 11:46:15 +0200

    formulaire

---
+++ bondecommande.php
@@ -0,0 +1,19 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Bon de commande</title>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+</head>
+<body>
+	<ul id="navigation">
+  			<li><a href="index.php" title="aller à Les bières">Les Bières</a></li>
+  			<li><a href="bondecommande.php" title="aller à Bon de commande">Bon de commande</a></li>
+	</ul>
+	
+
+<div class="container">
+	
+</body>
+</html>
